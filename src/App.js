cb48c19f1826280b05dcd0c1a581b9f9c62cfe79
sahilsolanki16 <sahilsolanki.studenttdb@gmail.com>
2024-05-19 21:12:43 +0530

    save first page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,226 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    {/* nav bar part start  */}
+    <div className='img1'>
+     <nav className="navbar navbar-expand-sm navbar-dark">
+  <div className="container-fluid ">
+    <div className='col-md-1'></div>
+    <a className="navbar-brand  text-md-center " href="javascript:void(0)"><img src='img/logo-white-1-60x60.png' className='col-md-12 col-3 ' /></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className='col-md-2'></div>
+    <div className="collapse navbar-collapse " id="mynavbar">
+      <ul className="navbar-nav me-auto  mx-auto">
+        <li className="nav-item">
+          <a className="nav-link fs-5 ms-md-5 text-light"href="javascript:void(0)">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link ms-md-4 fs-5 text-light" href="javascript:void(0)">Abput</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link ms-md-4 fs-5 text-light" href="javascript:void(0)">Service</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link ms-md-4 fs-5 text-light" href="javascript:void(0)">Contact</a>
+        </li>
+        <li className="nav-item">
+          <button className='ms-md-5 mt-md-2 border px-4 py-1 rounded-2 bg-t fs-5 bg-dgr text-light '>202-255-7890</button>
+          </li>
+      </ul>
     </div>
+  </div>
+</nav>
+<div className='h200'></div>
+<div className='container-fluid'>
+    <div className='row'>
+    <div className='col-md-6  mx-auto'>
+        <h1 className='text-light text-center display-3 ms-md-3 '>Once you choose hope, anything is possible...</h1>
+        <center><button className='bg-g border border-0 text-light  px-5  py-3 rounded-2 mt-md-3 fs-5'>Get Started</button></center>
+        <div className='h240'></div>
+
+    </div>
+  </div>
+</div>
+</div>
+<div className='h50'></div>
+
+{/* navbar part div closed  */}
+ 
+                                                {/*2nd part div start   */}
+<div className='container'>
+  <div className='row'>
+    <div className='col-md-12 '>
+      <p className='text-center'>CERTIFIED WEB DESIGNER BASED IN OAKLAND, CALIFORNIA</p>
+      <h1 className='text-center display-5'><b>Problems I can help with</b></h1>
+      <center><hr className='col-md-1 border-3 clg '/></center>
+    </div>
+</div>
+</div>
+
+
+<div className='container'>
+  <div className='row'>
+    <div className='col-md-1'></div>
+  <div className='col-md-3 col-12'>
+        <img src='img/2-home.jpg' className='col-md-12 col-12 rounded-2 ms-md-5'></img>
+        <p className='ms-md-5 fs-3'>Web Design</p>
+        <p className='ms-md-5 fs18' >Focus on how you can help and benefit your user. Use simple words to avoid confusion.</p>
+    </div>
+    <div className='col-md-3 col-12'>
+
+        <img src='img/3-home.jpg' className='col-md-12 col-12 rounded-2 ms-md-5'></img>
+        <p className='ms-md-5 fs-3'>Graphic Design</p>
+        <p className='ms-md-5 fs18' >Focus on how you can help and benefit your user. Use simple words to avoid confusion.</p>
+    </div>
+    <div className='col-md-3 col-12'>
+        <img src='img/4-home.jpg' className='col-md-12 col-12 ms-md-5 rounded-2 '></img>
+        <p className='ms-md-5 fs-3'>Content Creation </p>
+        <p className='ms-md-5 fs18' >Focus on how you can help and benefit your user. Use simple words to avoid confusion.</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<div className='h100'></div>
+
+                                         {/* 3rd part div start  */}                                               
+
+
+    <div className='container-fluid bg-ow '>
+      <div className='row'>
+        <div className='col-md-4  mx-auto'>
+          <center>
+          <div className='col-md-2 mx-auto'>
+        <img src='img/5-home.jpg' className='col-md-10 col-2 mt-5 rounded-circle'></img>
+        </div>
+        </center>
+        <p className='text-center mt-2 mb-0'>Ruth A. Flora</p>
+        <center>
+        <img src='img/d1.svg' className='h10'></img>
+        <img src='img/d1.svg' className='h10'></img>
+        <img src='img/d1.svg' className='h10'></img>
+        <img src='img/d1.svg' className='h10'></img>
+        <img src='img/d1.svg' className='h10'></img></center>
+
+        <p className='text-center fs-4 mt-3 mb-md-4 '>You can showcase a client review here. Make it nice and short, then use this button to send them to your reviews or services page.</p>
+       <center> <button className='border fs-5 px-4 border border-dark py-2 rounded-3 bg-dg'>What My Clients Say</button></center>
+
+       <div className='h100'></div>
+
+        </div>
+      </div>
+    </div>
+
+<div className='h200'></div>
+
+
+
+
+
+
+
+                                         {/* 4th part div start  */}                                               
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-1'></div>
+          <div className='col-md-4 col-12'>
+                  <img src='img/6-home.jpg' className='col-md-11 col-12 rounded-2 '></img>
+                  <p className='text-center fs-3 me-5 mb-2'>Dragos Andrei Atudorei</p>
+                  <p className='text-center fs16 me-5'>WordPress Content Creator <br />
+                      Certified Web Designer <br /> 
+                       Incurable Perfectionist</p>
+          </div>
+          <div className='col-md-6 col-12'>
+            <p className='mt-4'>WELCOME!</p>
+            <h1 className='display-5 text-md-start '><b>My name is Andrei and I am the creator of this template.</b></h1>
+            <p className='fs25 mt-3 '>I gave it my all to make this the best design to start from. <b>I named it Hope,</b> in honor of my client who inspired me to make it – her middle name is Hope.</p>
+            <p className='fs25'>For the first section at the top of the page, I recommend writing something that will <b> pique the interest </b> of your target audience. Make them curious to read more!</p>
+
+            <p className='fs25'>These middle sections are the perfect place to introduce yourself, and to perhaps start introducing <b>your services.</b></p>
+
+            <p className='fs25'>The last section below is great for a call to action.</p>
+
+            <button className=' py-2 fs18 bg-lg rounded-2 border col-md-5 col-12'>See My Services</button>
+            <button className=' py-2 fs18  rounded-2 border-2 border-black ms-md-4 bg-dg col-md-5 col-12 mt-md-0 mt-3'>More About Me</button>
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+      <div className='h200'></div>
+
+                                           {/* footer part div start  */}
+
+
+          <div className='container-fluid'>
+            <div className='row'>
+              <div className='col-md-12 img2'>
+                    <div className='h120'></div>
+                    <div className='col-md-6 mx-auto'>
+                    <h1 className='text-light display-2 text-center '>Insert a call to action</h1>
+                    <p className='text-light fs-2 text-center mt-3'>Think of what you want your visitor to do next. Maybe they should contact you?</p>
+                    <center><button className=' py-2 fs18  rounded-2 border col-md-3 col-4 bg-t bg-dgr mt-3'>Get In Touch</button></center>
+                    <div className='h120'></div>
+
+                    </div>
+              </div>
+            </div>
+          </div>
+          <div className='h50'></div>
+
+
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4 '>
+            <nav className="navbar navbar-expand-sm navbar-dark ">
+                    <ul className="navbar-nav me-auto mx-auto ">
+                      <li>
+                        <a className="nav-link text-dark fs-5"href="#">Home</a>
+                      </li>
+                      <li>
+                        <a className="nav-link ms-md-3 text-dark fs-5" href="#">Abput</a>
+                      </li>
+                      <li>
+                        <a className="nav-link ms-md-3 text-dark fs-5" href="#">Service</a>
+                      </li>
+                      <li>
+                        <a className="nav-link ms-md-3 text-dark fs-5" href="#">Contact</a>
+                      </li>
+                    </ul>
+                  
+              </nav>
+            </div>
+
+          <div className='col-md-4 col-12'>
+                <center><img src='img/logo-black-1.png' className='col-md-2 col-2 '></img></center>
+          </div>
+
+        <div className='col-md-4 col-12'>
+                <p className='fs-5 mt-3 ms-md-5 text-center'>© 2024 Hope Starter Design</p>
+        </div>
+          </div>
+        </div>
+        <div className='h50'></div>
+
+
+
+
+    </>
   );
 }
 
